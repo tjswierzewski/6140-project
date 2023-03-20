b3bfc57af413d9b02194fe80a02f985f7d011ba2
tjswierzewski <tjswierzewski@gmail.com>
2023-03-20 18:00:06 -0400

    use bisect method

--- create_numpy_from_data.py
+++ create_numpy_from_data.py
@@ -1,3 +1,4 @@
+import bisect
 import json
 import numpy
 
@@ -15,49 +16,38 @@ class Song:
     def from_MFD(cls, song):
         return cls(song["track_name"], cls.truncate_uri(song["track_uri"]))
     
+    def __eq__(self, __o: object) -> bool:
+        return self.hash == __o.hash
+    
+    def __lt__(self, __o: object) -> bool:
+        return self.hash < __o.hash
+
+    def __le__(self, __o: object) -> bool:
+        return self.hash <= __o.hash
+
+    def __gt__(self, __o: object) -> bool:
+        return self.hash > __o.hash
+
+    def __ge__(self, __o: object) -> bool:
+        return self.hash >= __o.hash
+
+    
     
 class SongList:
     def __init__(self) -> None:
         self.list = []
     
-    def binary_search(self,item_uri):
-        low = 0
-        high = len(self.list) - 1
-        mid = 0
-        check = hash(item_uri)
-
-        while low <= high:
-
-            mid = high + low // 2
-
-            if self.list[mid].hash < check:
-                low = mid + 1
-            elif self.list[mid].hash > check:
-                high = mid - 1
-            else:
-                return mid
-        return mid - 1
-    
     def insert(self, song):
-        if len(self.list) != 0: 
-            index = self.binary_search(song)
-            if song.hash == self.list[index].hash:
-                return
-        else:
-            index = 0
-        self.list.insert(index, song)
+        bisect.insort_left(self.list, song)
 
     def search(self, song_uri):
-        if len(self.list) == 0:
-            return None
-        index = self.binary_search(song_uri)
-        song = self.list[index]
-        if song.hash == hash(song_uri):
-            return song
+        index = bisect.bisect_left(self.list, hash(song_uri), key = lambda x: x.hash)
+        if len(self.list) != 0 and index != len(self.list) and self.list[index].uri == song_uri:
+            return index
         return None
+        
 
-
-def main(depth = 1, path = ".."):
+def main(depth = 5, path = ".."):
     start = 0
     stop = 999
     step = 1000
