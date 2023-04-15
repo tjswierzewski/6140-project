c2b994c03cc1190cf1d7b5cfa426f2e23a6ac38d
tjswierzewski <tjswierzewski@gmail.com>
2023-04-15 09:47:07 -0400

    Model class created

---
+++ two_tower_encoder.py
@@ -0,0 +1,48 @@
+from torch import matmul, nn, functional as F
+
+class User_Item_Encoder(nn.Module):
+    def __init__(self, user_features, item_features, layers, item_layers = None) -> None:
+        super(User_Item_Encoder, self).__init__()
+        user_layers = layers
+        if item_layers == None:
+            item_layers = layers
+
+        user_encoder_layers = []
+        user_encoder_layers.append(nn.Linear(user_features, user_layers[0]))
+        
+        for i in range(len(user_layers[0:-1])):
+            user_encoder_layers.append(nn.ReLU())
+            user_encoder_layers.append(nn.Linear(user_layers[i], user_layers[i+1]))
+        user_encoder_layers.append(nn.Sigmoid())
+        
+        item_encoder_layers = []
+        item_encoder_layers.append(nn.Linear(item_features, item_layers[0]))
+        
+        for i in range(len(item_layers[0:-1])):
+            item_encoder_layers.append(nn.ReLU())
+            item_encoder_layers.append(nn.Linear(item_layers[i], item_layers[i+1]))
+        item_encoder_layers.append(nn.Sigmoid())
+
+        self.user_encoder_functions = nn.Sequential(*user_encoder_layers)
+        self.item_encoder_functions = nn.Sequential(*item_encoder_layers)
+
+    def user_encoder(self, users):
+        return self.user_encoder_functions(users)
+
+    def item_encoder(self, items):
+        return self.item_encoder_functions(items)
+
+    def forward(self, users, items):
+        users = self.user_encoder(users)
+        items = self.item_encoder(items)
+
+        return matmul(F.normalize(users), F.normalize(items))
+
+
+def main():
+    model = User_Item_Encoder(100,500, [128,64,32])
+    
+
+if __name__ == "__main__":
+    main()
+
