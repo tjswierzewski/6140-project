f98d99a4f084223edaea84ea271dd2d00502b289
tjswierzewski <tjswierzewski@gmail.com>
2023-04-22 11:55:12 -0400

    update dataset creator

--- create_datasets_TT.py
+++ create_datasets_TT.py
@@ -10,34 +10,37 @@ def main():
     # Script Argument Parser
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("-s", "--songlist", help = "Songlist Pickle", required= True)
     parser.add_argument("-m", "--matrix", help = "Data Matrix", required= True)
     parser.add_argument("-S", "--seed", help = "Seed", type= int, default= None)
+    parser.add_argument("-v", "--validation", help = "size of training data used for validation", type=float)
+    parser.add_argument("-f", "--features", help = "Number of features in MF", type=int, default=100)
 
     args = parser.parse_args()
-
-    # Import SongList
-    songlist = SongList()
-    songlist.load(args.songlist)
 
     matrix = sparse.load_npz(args.matrix)
 
     train, test = train_test_split(matrix, test_size=0.1, random_state=args.seed)
-    train, validate = train_test_split(train, test_size=0.25, random_state=args.seed)
+    if args.validation != None:
+        train, validate = train_test_split(train, test_size=args.validation, random_state=args.seed)
     test, keys = data_to_query_label(test)
 
     output = {}
-    train_playlist_features, train_song_features = computeFeatureVectors(swap_song_index_to_X(train, shape=(train.shape[0], matrix.max())), 100, 0.0001)
+    train_playlist_features, train_song_features = computeFeatureVectors(swap_song_index_to_X(train, shape=(train.shape[0], matrix.max())), args.features, 0.0001)
     output["train_playlist_features"] = train_playlist_features
     output["train_song_features"] = train_song_features
-    output["validate_playlist_features"] = predictPlaylistFeatures(swap_song_index_to_X(validate, shape=(validate.shape[0], matrix.max())), train_song_features)
-    output["test_playlist_features"] = predictPlaylistFeatures(swap_song_index_to_X(test, shape=(len(test), matrix.max())), train_song_features)
-    output["keys"] = keys
     output["train"] = train
-    output["validate"] = validate
+    if args.validation != None:
+        output["validate_playlist_features"] = predictPlaylistFeatures(swap_song_index_to_X(validate, shape=(validate.shape[0], matrix.max())), train_song_features)
+        output["validate"] = validate
+    output["test_playlist_features"] = predictPlaylistFeatures(swap_song_index_to_X(test, shape=(len(test), matrix.max())), train_song_features)
     output["test"] = test
+    output["keys"] = keys
 
-    with open("NN_data_D10.pickle", "wb") as file:
+    file_name = args.matrix.split("_")[-1].split(".")[0] + f"_F{args.features}"
+    if args.validation:
+        file_name += "_V"
+    file_name += ".pickle"
+    with open(file_name, "wb") as file:
             pickle.dump(output, file)
 
 
