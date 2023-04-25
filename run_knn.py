9c0b5698a8dcecac6c552f8261b6b7e71abfea89
tjswierzewski <tjswierzewski@gmail.com>
2023-04-24 23:02:05 -0400

    script updates

--- run_knn.py
+++ run_knn.py
@@ -13,14 +13,14 @@ test_sizes = [1, 1000]
 reducer = "binary"
 
 matrix = sparse.load_npz("UvS_sparse_matrix_D1000.npz")
+f = open("knn_out.txt", "w")
 
-
-print("--------KNN Recommender--------")
+f.write("--------KNN Recommender--------")
 for query, test_size in product(query_length, test_sizes):
-    print(f"\nQuery Length: {query} Test Length: {test_size}")
-    train, test = train_test_split(matrix, test_size = test_size)
+    f.write(f"\nQuery Length: {query} Test Length: {test_size}")
+    train, test = train_test_split(matrix, test_size = 0.01)
+    query_playlists, query_answers = data_to_query_label(test, query_length=query, max_return=test_size)
     Train = swap_song_index_to_X(train, shape=(matrix.shape[0], matrix.max()), reducer=reducer)
-    query_playlists, query_answers = data_to_query_label(test, query_length=query)
 
     item_recommendations, fit_time, recommend_time, reduce_time = get_song_based_recommendations(Train.T, query_playlists)
 
@@ -30,9 +30,10 @@ for query, test_size in product(query_length, test_sizes):
     counts = np.unique(np_item_based_scores, return_counts = True)
     no_click = counts[1][0] if counts[0][0] == 0 else 0
     no_find = counts[1][-1] if counts[0][-1] == 51 else 0
-    print(f"\nItem Based Average:\nR_Precision: {mean[0]}\nNormalized Discounted Cumulative Gain: {mean[1]}\nRecommended Song Clicks: {mean[2]}")
-    print(f"No Click: {no_click}\nNo Find: {no_find}")
-    print(f"Fit Time: {fit_time}\nRecommend Time: {recommend_time}\nReduce Time: {reduce_time}")
+    f.write(f"\nN = {len(query_playlists)}")
+    f.write(f"Item Based Average:\nR_Precision: {mean[0]}\nNormalized Discounted Cumulative Gain: {mean[1]}\nRecommended Song Clicks: {mean[2]}")
+    f.write(f"No Click: {no_click}\nNo Find: {no_find}")
+    f.write(f"Fit Time: {fit_time}\nRecommend Time: {recommend_time}\nReduce Time: {reduce_time}")
 
     train, test = train_test_split(matrix, test_size = test_size)
     Train = swap_song_index_to_X(train, shape=(matrix.shape[0], matrix.max()), reducer=reducer)
@@ -46,6 +47,7 @@ for query, test_size in product(query_length, test_sizes):
     counts = np.unique(np_item_based_scores, return_counts = True)
     no_click = counts[1][0] if counts[0][0] == 0 else 0
     no_find = counts[1][-1] if counts[0][-1] == 51 else 0
-    print(f"\nUser Based Average:\nR_Precision: {mean[0]}\nNormalized Discounted Cumulative Gain: {mean[1]}\nRecommended Song Clicks: {mean[2]}")
-    print(f"No Click: {no_click}\nNo Find: {no_find}")
-    print(f"Fit Time: {fit_time}\nRecommend Time: {recommend_time}\nReduce Time: {reduce_time}")
+    f.write(f"\nN = {len(query_playlists)}")
+    f.write(f"User Based Average:\nR_Precision: {mean[0]}\nNormalized Discounted Cumulative Gain: {mean[1]}\nRecommended Song Clicks: {mean[2]}")
+    f.write(f"No Click: {no_click}\nNo Find: {no_find}")
+    f.write(f"Fit Time: {fit_time}\nRecommend Time: {recommend_time}\nReduce Time: {reduce_time}")
--- run_mf.py
+++ run_mf.py
@@ -15,12 +15,13 @@ features = [10,50,100,250,500]
 reducer = "binary"
 
 matrix = sparse.load_npz("UvS_sparse_matrix_D1000.npz")
+f = open("mf_out.txt", "w")
 
-print("--------MF Recommender--------")
+f.write("--------MF Recommender--------")
 for query, test_size, feature in product(query_length, test_sizes, features):
-    print(f"\nQuery Length: {query} Test Length: {test_size} Latent Features: {feature}")
-    train, test = train_test_split(matrix, test_size = test_size)
-    test_queries, test_answers = data_to_query_label(test, query_length=query)
+    f.write(f"\nQuery Length: {query} Test Length: {test_size} Latent Features: {feature}")
+    train, test = train_test_split(matrix, test_size = 0.01)
+    test_queries, test_answers = data_to_query_label(test, query_length=query, max_return=test_size)
     Train = swap_song_index_to_X(train, shape=(train.shape[0], matrix.max()), reducer=reducer)
     Test = swap_song_index_to_X(test, shape=(test.shape[0], matrix.max()), reducer=reducer)
     compute_start = perf_counter_ns()
@@ -35,6 +36,7 @@ for query, test_size, feature in product(query_length, test_sizes, features):
     counts = np.unique(np_scores, return_counts = True)
     no_click = counts[1][0] if counts[0][0] == 0 else 0
     no_find = counts[1][-1] if counts[0][-1] == 51 else 0
-    print(f"\nItem Based Average:\nR_Precision: {mean[0]}\nNormalized Discounted Cumulative Gain: {mean[1]}\nRecommended Song Clicks: {mean[2]}")
-    print(f"No Click: {no_click}\nNo Find: {no_find}")
-    print(f"Compute Time: {compute_stop- compute_start}\nRecommend Time: {recommend_stop - recommend_start}")
+    f.write(f"\nN = {len(test_queries)}")
+    f.write(f"Item Based Average:\nR_Precision: {mean[0]}\nNormalized Discounted Cumulative Gain: {mean[1]}\nRecommended Song Clicks: {mean[2]}")
+    f.write(f"No Click: {no_click}\nNo Find: {no_find}")
+    f.write(f"Compute Time: {compute_stop- compute_start}\nRecommend Time: {recommend_stop - recommend_start}")
